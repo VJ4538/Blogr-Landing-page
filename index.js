333ec76905e047b0664640b6aada010dd654e384
Vincent <vjiang4538@gmail.com>
2021-03-19 21:55:44 -0400

    Added tablet and mobile design and respsonive nav

--- index.js
+++ index.js
@@ -6,7 +6,41 @@ $(document).ready(function(){
 
     nav_link.click(function(){
       let panelId=$(this).attr("data-panel");
+      // console.log(this);
+      // console.log(panelId);
       $(`.${panelId}i`).toggleClass("rotated");
-      $(`.${panelId}`).slideToggle(500);
+      $(`.${panelId}`).slideToggle(300);
     })
+
+    //nav bar mobile
+
+    let bar_nav=$('.bar');
+      bar_nav.click(function(){
+        $('.m-dropdown').slideToggle(300);
+        $('.bar-open').toggle(100);
+        $('.bar-close').toggle(100);
+      })
+
+      
+
+    let nav_link_m = $('.drop-title');
+    
+      nav_link_m.click(function(){
+      let mpanelId=$(this).attr("data-mpanel");
+      console.log(this);
+      // console.log(panelId);
+      $(`.${mpanelId}i`).toggleClass("rotated");
+      $(`.${mpanelId}`).slideToggle(300);
+      
+    })
+
+
+
+
 })
+
+
+
+
+
+  
